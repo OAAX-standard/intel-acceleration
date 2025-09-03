ff02f23b013bd99031cd9784ac81943190fcf4ca
Ayoub Assis <assis.ayoub@gmail.com>
2025-09-03 14:39:57 +0200

    migrate to c++ codebase
    Signed-off-by: Ayoub Assis <assis.ayoub@gmail.com>

--- runtime-library/build-runtimes.sh
+++ runtime-library/build-runtimes.sh
@@ -4,14 +4,21 @@ cd "$(dirname "$0")"
 
 BUILD_DIR="$(pwd)/build"
 ARTIFACTS_DIR="$(pwd)/artifacts"
+ROOT_DIR="$(pwd)/.."
+
 mkdir -p $BUILD_DIR
 rm -rf $ARTIFACTS_DIR
 mkdir -p $ARTIFACTS_DIR
 
+VERSION_FILE="$ROOT_DIR/VERSION"
+RUNTIME_VERSION="$(cat $VERSION_FILE)"
+
+echo "Building for runtime version: $RUNTIME_VERSION"
+
 cd ${BUILD_DIR}
 
 rm -rf *
-cmake .. -DPLATFORM=X86_64
+cmake .. -DPLATFORM=X86_64 -DCMAKE_BUILD_TYPE=Release -DRUNTIME_VERSION="$RUNTIME_VERSION"
 make -j
 echo "Build complete. The following shared libraries were created:"
 ls ./*.so
